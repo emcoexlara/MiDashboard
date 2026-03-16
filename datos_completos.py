60c2f704d5da6afd5daf36c1f68ceabe08ba0e29
EMCOEXLARA <emcoexlara@gmail.com>
2026-03-16 12:06:58 -0400

    Update datos_completos.py

--- datos_completos.py
+++ datos_completos.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 def run(BASE_DIR):
-    st.markdown("## Datos Completos")
+    st.markdown("## 🗂 Datos Completos")
 
     archivo = st.sidebar.file_uploader("Actualizar datos (Excel)", type=["xlsx"])
     if archivo:
@@ -12,12 +12,10 @@ def run(BASE_DIR):
         df = pd.read_excel(BASE_DIR / "datos.xlsx")
 
     df.columns = df.columns.str.strip().str.lower()
-
-    st.subheader("Tabla completa de operaciones")
     st.dataframe(df, use_container_width=True)
 
     st.download_button(
-        label="Descargar datos en CSV",
+        label="📥 Descargar CSV",
         data=df.to_csv(index=False),
         file_name="datos_comercio_exterior.csv",
         mime="text/csv"
