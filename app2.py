43f0dc6e72e723fc28bc6eabff4f2afdf12a0924
EMCOEXLARA <emcoexlara@gmail.com>
2026-03-25 09:11:11 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -223,7 +223,7 @@ if "kpi_mostrado" not in st.session_state:
     </style>
     """, unsafe_allow_html=True)
     
-     col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4 = st.columns(4)
 
     with col1:
         st.markdown(f"""
