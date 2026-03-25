6e64fa87fb2fb248c40e65f1a0204d05ad03898e
EMCOEXLARA <emcoexlara@gmail.com>
2026-03-25 08:44:32 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -289,8 +289,6 @@ with col4:
 # MÉTRICAS
 col1, col2, col3, col4 = st.columns(4)
 
-col1, col2, col3, col4 = st.columns(4)
-
 with col1:
     st.markdown(f"""
     <div style='background:{COLOR_TITULO}; padding:20px; border-radius:12px;
